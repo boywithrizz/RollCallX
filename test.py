9b4dbceaed9cdc9843762a8cda59f0d42999717d
boywithrizz <beatscupltors@gmail.com>
2024-12-29 00:41:56 +0530

    Working done till subject level

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from code import interact
 from hmac import new
 from math import floor
 import arrow
@@ -21,18 +22,21 @@ class subject:
         self.class_a = 0
         self.class_l = 0
         self.class_h = self.class_a + self.class_l
-        self.mt1leaves = 0
-        self.mt2leaves = 0
-        self.mt3leaves = 0
-        temp = universal.semstartdate
-        while temp < universal.mt3date:
-            self.mt3leaves += 1
-            if temp < universal.mt2date:
-                self.mt2leaves += 1
-            if temp < universal.mt1date:
-                self.mt1leaves += 1
-            temp = temp.shift(days=1)  # Update temp
-
+        self.mt1classes = 0
+        self.mt2classes = 0
+        self.mt3classes = 0
+        for i in new_totaldays:
+            if (i < universal.mt1date) :
+                self.mt1classes += 1
+            if (i < universal.mt2date) :
+                self.mt2classes += 1
+            if (i < universal.mt3date) :
+                self.mt3classes += 1
+        self.mt1leaves = floor(0.25*self.mt1classes)
+        self.mt2leaves = floor(0.25*self.mt2classes)
+        self.mt3leaves = floor(0.25*self.mt3classes)
+            
+            
 def date(strdate):
     ourdate = arrow.get(strdate, "DD-MM-YY")
     return ourdate
@@ -76,12 +80,43 @@ def final(totaldays,exclusions):
             new_totaldays.append(i)
     return new_totaldays
 
+def main():
+    a = 1
+    today = date("01-01-25")
+    att = 1
+    itemlist = []
+    while (a!=0):
+        if (today <= universal.mt1date):
+            total_leaves = math.mt1leaves
+        if (today > universal.mt1date and today <= universal.mt2date):
+            total_leaves = math.mt2leaves
+        if (today > universal.mt2date):
+            total_leaves = math.mt3leaves
+        print(f"Today's date is {today}")
+        if today in math.new_totaldays:
+            att = int(input("Today is maths period mark attendance 0 or 1 :"))
+            if att==1:
+                math.class_a += 1
+            else:
+                math.class_l += 1
+        else :
+            print("No maths period today!")
+        remaining_leaves = total_leaves - math.class_l
+        itemlist = {"remaining_leaves" : remaining_leaves, "class_a" : math.class_a, "class_l" : math.class_l}
+        print(itemlist)
+        a = int(input("Continue 0 (no) or 1 (next) or 2 (random date):"))
+        if (a==1):
+            today = today.shift(days=1)
+        else:
+            today = date(input("Enter the next date :"))
+
+
 exclusions = f_exclusions()
 weeklist = f_weeklist()
 universal = Universal("01-01-25", "12-03-25", "17-03-25", "12-04-25", "17-04-25", "12-05-25",exclusions)
 totaldays = numdate_multi(universal.semstartdate, universal.mt3date, weeklist)
 newtotaldays = final(totaldays,exclusions)
 math = subject(universal,weeklist,newtotaldays)
-print(vars(math))
+main()
 
 
