b52c2cd9887e6a20da5a7f7e7e1f061c6536c60e
boywithrizz <beatscupltors@gmail.com>
2024-12-28 15:36:38 +0530

    deepak kalal prank

--- main.py
+++ main.py
@@ -1,26 +1,17 @@
-#!/usr/bin/python
-
-# This is a simple echo bot using the decorator mechanism.
-# It echoes any incoming text messages.
-import asyncio
-
 from telebot.async_telebot import AsyncTeleBot
+import asyncio
+# Replace with your bot token
+TOKEN = '7551528861:AAEpxbtxw9cOhBbE0-ldqU2u8RowHflK9ZE'
+bot = AsyncTeleBot(TOKEN)
 
-bot = AsyncTeleBot('7551528861:AAEpxbtxw9cOhBbE0-ldqU2u8RowHflK9ZE')
-
-
-# Handle '/start' and '/help'
-@bot.message_handler(commands=['help', 'start'])
-async def send_welcome(message):
+@bot.message_handler(commands=['start','help'])
+async def info(message):
     user = message.from_user
-    text = f'Hi {user.first_name}, I am EchoBot.\nJust write me something and I will repeat it!'
-    await bot.reply_to(message, text)
+    await bot.reply_to(message,f'Hello {user.first_name} {user.last_name},\nThis bot is made by Ayush Yadav\nThis bot will give you a reward when you type send some message to it, Try it!')
+@bot.message_handler(func=lambda message : True)
+async def prank(message):
+    with open("life-me.mp4","rb") as video:
+        await bot.send_animation(message.chat.id,video,caption='Padhle jaakr saale!')
 
-
-# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
-@bot.message_handler(func=lambda message: True)
-async def echo_message(message):
-    await bot.reply_to(message, message.text)
-
-
+# Start polling
 asyncio.run(bot.polling())
